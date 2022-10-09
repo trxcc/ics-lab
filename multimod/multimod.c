cf857653556373f6c7bcc8ce571272d5dd6d8d62
tracer-nju <tracer@nju.edu.cn>
2022-10-09 16:30:57 +0800

    Static hostname: trxcc-virtual-machine        Icon name: computer-vm          Chassis: vm       Machine ID: dec19470aab8474690a4bc58117a7f2a          Boot ID: 052da9f8c7a44a5a81e9c050f8c19a46   Virtualization: vmware Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-48-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  16:30:57 up  4:34,  6 users,  load average: 0.00, 0.03, 0.02

--- multimod/multimod.c
+++ multimod/multimod.c
@@ -1,6 +1,5 @@
 #include <stdint.h>
 #include <stdio.h>
-static const uint64_t N = -1ULL;
 
 uint64_t mod(uint64_t a, uint64_t b) {
   while (a >= b) {
@@ -17,18 +16,16 @@ uint64_t Constant(uint64_t m) {
     ans = mod(mod(ans, m) + mod(t, m), m);
     t <<= 1;
     t = mod(t, m);
-    //t = mod(mod(t, m) + mod(1, m), m);
   }
   return mod(mod(ans, m) + mod(1, m), m);
 }
 
-static uint64_t plusmod(uint64_t x, uint64_t y, uint64_t m){
+uint64_t plusmod(uint64_t x, uint64_t y, uint64_t m){
   uint64_t t = x + y;
   return mod((mod(t, m)+Constant(m)), m);
 }
 
 uint64_t multimod(uint64_t a, uint64_t b, uint64_t m) {
-  uint64_t t = a + b;
   uint64_t ans = 0, tmp = b;
   while(a != 0)  {
     uint64_t a0 = mod(a, 2);
