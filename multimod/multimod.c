a147f7839cfbb42c5a64273cab7176fa0ff90008
tracer-nju <tracer@nju.edu.cn>
2022-10-09 15:54:20 +0800

    Static hostname: trxcc-virtual-machine        Icon name: computer-vm          Chassis: vm       Machine ID: dec19470aab8474690a4bc58117a7f2a          Boot ID: 052da9f8c7a44a5a81e9c050f8c19a46   Virtualization: vmware Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-48-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  15:54:20 up  3:58,  6 users,  load average: 0.00, 0.00, 0.00

--- multimod/multimod.c
+++ multimod/multimod.c
@@ -14,8 +14,7 @@ uint64_t Constant(uint64_t m) {
   uint64_t t = 1;
   uint64_t ans = 0;
   while(flag--) {
-    #define U64 "%" PRIu64
-    printf("t:" U64 "\n", t);
+    printf("%lu\n", t);
     ans = mod(mod(ans, m) + mod(t, m), m);
     uint64_t tmp = t;
     t <<= 1;
