48e0220b5470499fd20bd4cffe617c3909b673f1
tracer-nju <tracer@nju.edu.cn>
2022-11-29 19:13:57 +0800

    Static hostname: trxcc-virtual-machine        Icon name: computer-vm          Chassis: vm       Machine ID: dec19470aab8474690a4bc58117a7f2a          Boot ID: 5d577252ce0a4ea89e995e00cb70db7a   Virtualization: vmware Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-53-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  19:13:57 up  3:30,  6 users,  load average: 4.00, 4.02, 4.00

--- perftune/sieve.c
+++ perftune/sieve.c
@@ -5,19 +5,20 @@
 
 #define N 10000000
 
-static bool is_not_prime[N];
+static bool is_prime[N];
 static int  primes[N];
 
 int *sieve(int n) {
-  //assert(n + 1 < N);
-/*
-  for (int i = 3; i <= n; i += 2)
+  assert(n + 1 < N);
+
+  const int MAXN = (n >> 1) + 1;
+  for (int i = 1; i <= MAXN; i++)
     is_prime[i] = true;
    
-  for (int i = 3; (unsigned long long)i * i <= n; i += 2) {
-    if (is_prime[i]) {
-      for (int j = i * i; j <= n; j += i) {
-        is_prime[j] = false;
+  for (int i = 3; i * i <= n; i += 2) {
+    if (is_prime[i >> 1]) {
+      for (int j = i * i; j <= n; j += (i * 2)) {
+        is_prime[j >> 1] = false;
       }
     }
   }
@@ -26,26 +27,30 @@ int *sieve(int n) {
   *p++ = 2;
   for (int i = 3; i <= n; i += 2)
     if (is_prime[i]) {
-      *p++ = i;
+      *p++ = i * 2 + 1;
     }
   *p = 0;
   return primes;
-*/
-  
 
-  int cnt = 0;
+  
+/*
+  int *p = primes, *tmp = primes;
   for (int i = 2; i <= n; i++) {
     if (!is_not_prime[i]) {
-      primes[cnt++] = i;
+      *p++ = i;
     }
-    for (int j = 0; j < cnt; j++) {
-      if (i * primes[j] > n) break;
-      is_not_prime[i * primes[j]] = true;
-      if (i % primes[j] == 0) {
+    tmp = primes;
+    while (tmp != p) {
+      if (i * (*tmp) > n) {
         break;
       }
+      is_not_prime[i * (*tmp)] = true;
+      if (!(i % (*tmp))) {
+        break;
+      }
+      tmp ++;
     }
   }
   return primes;
-  
+*/
 }
